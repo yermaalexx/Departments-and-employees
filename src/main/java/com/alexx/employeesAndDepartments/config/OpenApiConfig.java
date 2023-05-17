ea86931c0a9c8899681c5ab833296491f359a6a6
Yermakov Olexii <yermakov80@gmail.com>
2023-05-17 21:56:55 +0300

    created class ProjectConfig and moved class ErrorDetails

--- src/main/java/com/alexx/employeesAndDepartments/config/OpenApiConfig.java
+++ src/main/java/com/alexx/employeesAndDepartments/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.alexx.employeesAndDepartments.openAPI;
+package com.alexx.employeesAndDepartments.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
